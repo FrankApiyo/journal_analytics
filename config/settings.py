5c9ac4b8f7a9c6737c5fcecbda800cc052697b18
FrankApiyo <franklineapiyo@gmail.com>
2025-03-23 15:52:16 +0300

    feat: Add the user document model, DB configs and migrations

--- config/settings.py
+++ config/settings.py
@@ -83,10 +83,15 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "live_view_svelte_offline_demo_dev",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "postgres",  # Must match the service name in docker-compose.yml
+        "PORT": "5432",
     }
 }
 
--- documents/models.py
+++ documents/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class UserDocument(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50000)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.name)
